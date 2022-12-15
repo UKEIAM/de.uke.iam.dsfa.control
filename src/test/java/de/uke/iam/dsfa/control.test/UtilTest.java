c88a820d7df4111d4b162f52723d5e573687f7a5
Omar Fulla <o.fulla@uke.de>
2022-12-15 13:48:11 +0100

    DSFA-101 add ExcelReaderTest and ExcelReaderWithErrorsTest to TestFile

--- src/test/java/de/uke/iam/dsfa/control.test/UtilTest.java
+++ src/test/java/de/uke/iam/dsfa/control.test/UtilTest.java
@@ -2,21 +2,20 @@ package de.uke.iam.dsfa.control.test;
 
 
 import de.samply.config.util.FileFinderUtil;
-import de.uke.iam.dsfa.control.db.DatabaseConfiguration;
+import de.uke.iam.dsfa.control.model.ExcelReaderResponse;
 import de.uke.iam.dsfa.control.util.ExcelReader;
 import de.uke.iam.dsfa.control.util.WordWriter;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class UtilTest  {
-    static DSLContext dsl = DatabaseConfiguration.get().getDsl();
     private Logger logger = LoggerFactory.getLogger(UtilTest.class);
     @Test
     public void ExcelReaderTest() throws FileNotFoundException {
@@ -24,8 +23,24 @@ public class UtilTest  {
         File excelFile = FileFinderUtil.findFile("example.xlsx");
 
         logger.debug("Found file: " + excelFile.getAbsolutePath());
+        FileInputStream file = ExcelReader.fileToInputStream(excelFile);
+        ExcelReader.readFile(file);
+        ExcelReaderResponse response = ExcelReader.getResponse();
+        System.out.println(response.getStatus());
+    }
+    @Test
+    public void ExcelReaderWithErrorsTest() throws FileNotFoundException {
+        // Nutzt bitte den Filefinder oder den Classloader, um Dateien aus "resources" zu laden
+        File excelFile = FileFinderUtil.findFile("exampleWithErrors.xlsx");
 
-        ExcelReader.readFile(excelFile,"target/");
+        logger.debug("Found file: " + excelFile.getAbsolutePath());
+        FileInputStream file = ExcelReader.fileToInputStream(excelFile);
+        ExcelReader.readFile(file);
+        ExcelReaderResponse response = ExcelReader.getResponse();
+        System.out.println(response.getStatus());
+        for (String comment : response.getComments()) {
+            System.out.println(comment);
+        }
     }
 
     @Test
